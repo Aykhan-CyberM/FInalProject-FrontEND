c113fed2d646d3323a0ef617ab6dd2b4c29118e2
Nurlan <nurlangn@code.edu.az>
2023-05-28 03:53:13 +0400

    some detail added to top

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,44 +5,68 @@ const engSection = document.querySelector("#English")
 const usdSection = document.querySelector("#USD")
 const language = document.querySelector(".Language__Dropdown")
 const currency = document.querySelector(".usd__Dropdown")
+const show =  document.querySelector(".show")
+const topAnimation = document.querySelector(".showAnimation")
+
+const topNavDropdownSellections = document.querySelector(".sellections").querySelectorAll("p");
+topNavDropdownSellections.forEach((links) => links.addEventListener('click', () => {
+    links.classList.add(show)
+}));
+
 // brands hoverAndActive top navbar
-
-
 usdSection.addEventListener('click', () => {
-    makeActive(usd, "Rotate", usdSection)
-    displayElement(currency)
+    makeAcrtiveDropdowns(usd, "Rotate", usdSection,currency)
+    makeActive(currency, "topAnimation")
 })
 
 engSection.addEventListener('click', () => {
-    makeActive(eng, "Rotate", engSection);
-    displayElement(language)
+    makeAcrtiveDropdowns(eng, "Rotate", engSection,language);
+    if (language.style.top != "55px" ){ 
+        language.style.top = "55px"
+        language.style.opacity = "1"
+    } else {
+        language.style.top = "80px"
+        language.style.opacity = "0"
+    }
 });
 
 brandContainer.forEach(btn=> btn.addEventListener('click',()=>{
-    makeActive(btn, "activeIcon", btn)
+    makeActive(btn, "activeIcon")
 }))
 
 
 // activeFunction
-function displayElement(obj) {
-    if (obj.style.visibility != "visible") {
-        obj.style.visibility = "visible"
+
+function AddAnimation(){
+
+}
+ 
+function makeActive(Element, active) {
+    if (Element.classList.contains(active) ){
+        Element.classList.remove(active);
+    } else {
+        Element.classList.add(active);
     }
-    else {
-        obj.style.visibility = "hidden";
-    }
+    document.addEventListener('click', (event) => {
+        if (!Element.contains(event.target)){
+            Element.classList.remove(active)
+        }
+    })
 }
 
-function makeActive(obj, active, container) {
-    if (obj.classList.contains(active)) {
-        obj.classList.remove(active);
-    } else {
-        obj.classList.add(active);
-    }
 
+function makeAcrtiveDropdowns(Element, active, container,show) {
+    if (Element.classList.contains(active) && show.style.visibility == "visible") {
+        Element.classList.remove(active);
+        show.style.visibility = "hidden"
+    } else {
+        Element.classList.add(active);
+        show.style.visibility = "visible";
+    }
     document.addEventListener('click', (event) => {
-        if (!container.contains(event.target)) {
-            obj.classList.remove(active)
+        if (!container.contains(event.target) && !show.contains(event.target)) {
+            Element.classList.remove(active)
+            show.style.visibility = "hidden";
         }
     })
 }
