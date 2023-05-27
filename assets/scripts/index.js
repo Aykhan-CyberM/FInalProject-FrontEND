283409d8012516c224d9b195c8fe0925f6a56e1f
Nurlan <nurlangn@code.edu.az>
2023-05-28 01:27:37 +0400

    top navbar dropdown and aos added

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,23 +1,54 @@
 const brandFacebook = document.querySelector(".brand_facebook")
 const brandTwitter = document.querySelector(".brand_twitter")
+const eng = document.querySelector("#eng")
+const usd = document.querySelector("#usd")
+const engSection = document.querySelector("#English")
+const usdSection = document.querySelector("#USD")
+const language = document.querySelector(".Language__Dropdown")
+const currency = document.querySelector(".usd__Dropdown")
+
+// brands hoverAndActive top navbar
 brandTwitter.addEventListener("click", () => {
-    makeActive(brandTwitter, "activeIcon")
+    makeActive(brandTwitter, "activeIcon", brandTwitter)
 })
+
 brandFacebook.addEventListener("click", () => {
-    makeActive(brandFacebook, "activeIcon")
-    
+    makeActive(brandFacebook, "activeIcon", brandFacebook)
 })
-function makeActive(obj, active) {
+
+usdSection.addEventListener('click', () => {
+    makeActive(usd, "Rotate", usdSection)
+    displayElement(currency)
+})
+
+engSection.addEventListener('click', () => {
+    makeActive(eng, "Rotate", engSection);
+    displayElement(language)
+});
+
+
+
+// activeFunction
+function displayElement(obj) {
+    if (obj.style.visibility != "visible") {
+        obj.style.visibility = "visible"
+    }
+    else {
+        obj.style.visibility = "hidden";
+    }
+}
+
+function makeActive(obj, active, container) {
     if (obj.classList.contains(active)) {
         obj.classList.remove(active);
     } else {
         obj.classList.add(active);
     }
-    
-   document.addEventListener('click',(event)=>{
-    if(!obj.contains(event.target)){
-        obj.classList.remove(active)
-    }
-   })
+
+    document.addEventListener('click', (event) => {
+        if (!container.contains(event.target)) {
+            obj.classList.remove(active)
+        }
+    })
 }
 
