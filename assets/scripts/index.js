0e8d4cd3b315c298aa300fe92ca09bab3b97e84a
Nurlan <nurlangn@code.edu.az>
2023-05-29 00:46:23 +0400

    nav dropdown

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,10 +12,34 @@ const mainNavDropdownSellections = document.querySelector(".main__dropdowns").qu
 const homebtn = document.querySelector('#home')
 const homeDropdown = document.querySelector('.home__Dropdown')
 const homeArrow = document.querySelector('#homeArrow')
+const shopbtn = document.querySelector('#shop')
+const shopDropdown = document.querySelector('.shop__Dropdown')
+const shopArrow = document.querySelector('#shopArrow')
+const iconsbtn = document.querySelector('#icons')
+const iconsDropdown = document.querySelector('.icons__Dropdown')
+const iconsArrow = document.querySelector('#iconsArrow')
+const pagesbtn = document.querySelector('#pages')
+const pagesDropdown = document.querySelector('.pages__Dropdown')
+const pagesArrow = document.querySelector('#pagesArrow')
+const docsbtn = document.querySelector('#docs')
+const docsDropdown = document.querySelector('.docs__Dropdown')
+const docsArrow = document.querySelector('#docsArrow')
 
 // brands hoverAndActive top navbar
+docsbtn.addEventListener('click',()=>{
+    makeAcrtiveDropdowns(docsArrow,"Rotate",docsbtn,docsDropdown,"DropAnimate2",mainNavDropdownSellections)
+})
+pagesbtn.addEventListener('click',()=>{
+    makeAcrtiveDropdowns(pagesArrow,"Rotate",pagesbtn,pagesDropdown,"DropAnimate2",mainNavDropdownSellections)
+})
+iconsbtn.addEventListener('click',()=>{
+    makeAcrtiveDropdowns(iconsArrow,"Rotate",iconsbtn,iconsDropdown,"DropAnimate2",mainNavDropdownSellections)
+})
 homebtn.addEventListener('click',()=>{
     makeAcrtiveDropdowns(homeArrow,"Rotate",homebtn,homeDropdown,"DropAnimate2",mainNavDropdownSellections)
+})
+shopbtn.addEventListener('click',()=>{
+    makeAcrtiveDropdowns(shopArrow,"Rotate",shopbtn,shopDropdown,"DropAnimate2",mainNavDropdownSellections)
 })
 
 usdSection.addEventListener('click', () => {
