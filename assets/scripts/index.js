ce3a475d1e8630035aea32f689043166b3dfbc6c
Nurlan <nurlangn@code.edu.az>
2023-05-29 17:01:51 +0400

    nav dropdown

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,54 +1,53 @@
-const eng = document.querySelector("#eng")
-const usd = document.querySelector("#usd")
-const brandContainer = document.querySelector(".icons").querySelectorAll("i")
-const engSection = document.querySelector("#English")
-const usdSection = document.querySelector("#USD")
+const engArrow = document.querySelector("#engArrow")
+const usdArrow = document.querySelector("#usdArrow")
+const brandContainer = document.querySelector(".top__navbar-socialMedia").querySelectorAll("i")
+const engSection = document.querySelector("#top__navbar-languageConatiner")
+const usdSection = document.querySelector("#top__navbar-curencyConatiner")
 const language = document.querySelector(".Language__Dropdown")
 const currency = document.querySelector(".usd__Dropdown")
-const show = document.querySelector(".show")
 const topAnimation = document.querySelector(".showAnimation")
-const topNavDropdownSellections = document.querySelector(".sellections").querySelectorAll("p");
-const mainNavDropdownSellections = document.querySelector(".main__dropdowns").querySelectorAll("p");
-const homebtn = document.querySelector('#home')
-const homeDropdown = document.querySelector('.home__Dropdown')
-const homeArrow = document.querySelector('#homeArrow')
-const shopbtn = document.querySelector('#shop')
-const shopDropdown = document.querySelector('.shop__Dropdown')
-const shopArrow = document.querySelector('#shopArrow')
-const iconsbtn = document.querySelector('#icons')
-const iconsDropdown = document.querySelector('.icons__Dropdown')
-const iconsArrow = document.querySelector('#iconsArrow')
-const pagesbtn = document.querySelector('#pages')
-const pagesDropdown = document.querySelector('.pages__Dropdown')
-const pagesArrow = document.querySelector('#pagesArrow')
-const docsbtn = document.querySelector('#docs')
-const docsDropdown = document.querySelector('.docs__Dropdown')
-const docsArrow = document.querySelector('#docsArrow')
+// const topNavDropdownSellections = document.querySelector(".sellections").querySelectorAll("p");
+// const mainNavDropdownSellections = document.querySelector(".main__dropdowns").querySelectorAll("p");
+// const homebtn = document.querySelector('#home')
+// const homeDropdown = document.querySelector('.home__Dropdown')
+// const homeArrow = document.querySelector('#homeArrow')
+// const shopbtn = document.querySelector('#shop')
+// const shopDropdown = document.querySelector('.shop__Dropdown')
+// const shopArrow = document.querySelector('#shopArrow')
+// const iconsbtn = document.querySelector('#icons')
+// const iconsDropdown = document.querySelector('.icons__Dropdown')
+// const iconsArrow = document.querySelector('#iconsArrow')
+// const pagesbtn = document.querySelector('#pages')
+// const pagesDropdown = document.querySelector('.pages__Dropdown')
+// const pagesArrow = document.querySelector('#pagesArrow')
+// const docsbtn = document.querySelector('#docs')
+// const docsDropdown = document.querySelector('.docs__Dropdown')
+// const docsArrow = document.querySelector('#docsArrow')
 
 // brands hoverAndActive top navbar
-docsbtn.addEventListener('click',()=>{
-    makeAcrtiveDropdowns(docsArrow,"Rotate",docsbtn,docsDropdown,"DropAnimate2",mainNavDropdownSellections)
-})
-pagesbtn.addEventListener('click',()=>{
-    makeAcrtiveDropdowns(pagesArrow,"Rotate",pagesbtn,pagesDropdown,"DropAnimate2",mainNavDropdownSellections)
-})
-iconsbtn.addEventListener('click',()=>{
-    makeAcrtiveDropdowns(iconsArrow,"Rotate",iconsbtn,iconsDropdown,"DropAnimate2",mainNavDropdownSellections)
-})
-homebtn.addEventListener('click',()=>{
-    makeAcrtiveDropdowns(homeArrow,"Rotate",homebtn,homeDropdown,"DropAnimate2",mainNavDropdownSellections)
-})
-shopbtn.addEventListener('click',()=>{
-    makeAcrtiveDropdowns(shopArrow,"Rotate",shopbtn,shopDropdown,"DropAnimate2",mainNavDropdownSellections)
-})
 
 usdSection.addEventListener('click', () => {
-    makeAcrtiveDropdowns(usd, "Rotate", usdSection, currency,"DropAnimate",topNavDropdownSellections)
+    makeAcrtiveDropdowns(usdArrow, "Rotate", usdSection, currency,"DropAnimate")
 })
 
 engSection.addEventListener('click', () => {
-    makeAcrtiveDropdowns(eng, "Rotate", engSection, language,"DropAnimate",topNavDropdownSellections);
+    makeAcrtiveDropdowns(engArrow, "Rotate", engSection, language,"DropAnimate");
 });
+// docsbtn.addEventListener('click',()=>{
+//     makeAcrtiveDropdowns(docsArrow,"Rotate",docsbtn,docsDropdown,"DropAnimate2",mainNavDropdownSellections)
+// })
+// pagesbtn.addEventListener('click',()=>{
+//     makeAcrtiveDropdowns(pagesArrow,"Rotate",pagesbtn,pagesDropdown,"DropAnimate2",mainNavDropdownSellections)
+// })
+// iconsbtn.addEventListener('click',()=>{
+//     makeAcrtiveDropdowns(iconsArrow,"Rotate",iconsbtn,iconsDropdown,"DropAnimate2",mainNavDropdownSellections)
+// })
+// homebtn.addEventListener('click',()=>{
+//     makeAcrtiveDropdowns(homeArrow,"Rotate",homebtn,homeDropdown,"DropAnimate2",mainNavDropdownSellections)
+// })
+// shopbtn.addEventListener('click',()=>{
+//     makeAcrtiveDropdowns(shopArrow,"Rotate",shopbtn,shopDropdown,"DropAnimate2",mainNavDropdownSellections)
+// })
 
 brandContainer.forEach(btn => btn.addEventListener('click', () => {
     makeActive(btn, "activeIcon")
@@ -70,7 +69,7 @@ function makeActive(Element, active) {
     })
 }
 
-function makeAcrtiveDropdowns(Element, active, container, show,Animation,sellections) {
+function makeAcrtiveDropdowns(Element, active, container, show,Animation) {
     if (Element.classList.contains(active) && show.style.visibility == "visible") {
         Element.classList.remove(active);
         show.classList.remove(Animation);
